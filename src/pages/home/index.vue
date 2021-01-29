29475b64a0d1296d44eeb08a5fd83e3296a038b7
yuli <yuli@xdja.com>
2021-01-29 17:28:13 +0800

    uni-ui的安装以及使用示例

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,15 +3,21 @@
  * @Author: yuli
  * @Date: 2021-01-29 15:30:35
  * @LastEditors: yuli
- * @LastEditTime: 2021-01-29 17:10:29
+ * @LastEditTime: 2021-01-29 17:26:49
 -->
 <template>
   <view>
     首页
     <text class="iconfont iconvideocamera"></text>
+    <uni-badge text="1"></uni-badge>
+    <uni-badge text="2" type="primary"></uni-badge>
+    <uni-badge text="3" type="success"></uni-badge>
   </view>
 </template>
 <script>
-export default {}
+import { uniBadge } from "@dcloudio/uni-ui"
+export default {
+  components: { uniBadge },
+}
 </script>
 <style lang="scss"></style>
